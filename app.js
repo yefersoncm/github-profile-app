779f35b504e3de37909721f388653c532da99a77
yefersoncm <yefersoncm@gmail.com>
2021-03-27 09:16:40 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const APIURL = 'https://api.github.com/users/';
 const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
-const numRepos = 20;
+const numRepos = 17;
 getUser('yefersoncm');
 
 async function getUser(username){
